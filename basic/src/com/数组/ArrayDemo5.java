c97dc343f967154d5b3542a6afd5b5e5b649c1c0
zhangxinxin <1285367184@qq.com>
2023-11-01 22:05:53 +0800

    数组

---
+++ basic/src/com/数组/ArrayDemo5.java
@@ -0,0 +1,40 @@
+package com.数组;
+
+public class ArrayDemo5 {
+    public static void main(String[] args) {
+
+        // 数组的动态初始化和常见问题
+
+        // 1.数组动态初始化格式: 数据类型[] 数组名 = new 数组类型[数组长度];
+        // 在动态创建数组的时候,需要手动指定数组的长度再由虚拟机给出默认的初始化值
+        /* 数组默认初始化值的规律:
+            byte short int long  => 0
+            float double => 0.0
+            char => '\u0000'
+            boolean => false
+            引用数据类型: String => null
+         */
+
+
+        // 2.存储50个学生姓名,但是学生名字是未知的
+        String[] names = new String[50];
+
+        // 添加学生
+        names[0] = "zxx";
+        names[1] = "xin";
+
+        // 获取值
+        System.out.println(names[0]); // zxx
+        System.out.println(names[1]); // xin
+        System.out.println(names[2]); // null
+
+        // 3.测试数组默认数据类型
+        int[] testArr = new int[3];
+        System.out.println(testArr[0]); // 0
+        System.out.println(testArr[1]); // 0
+        System.out.println(testArr[2]); // 0
+
+        // 4.索引越界异常
+        System.out.println(testArr[10]); //Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 3 at com.数组.ArrayDemo5.main(ArrayDemo5.java:38)
+    }
+}
---
+++ basic/src/com/数组/ArrayDemo6.java
@@ -0,0 +1,63 @@
+package com.数组;
+
+import java.util.Random;
+
+public class ArrayDemo6 {
+    public static void main(String[] args) {
+
+        // 1.求最大值
+        int[] nums = { 33, 5, 22, 44, 55, 18 };
+        int max = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > max) max = nums[i];
+        }
+
+        System.out.println(max);
+
+        // 2.生成10个随机数,求和
+        int[] nums2 = new int[10];
+        Random r = new Random();
+        int sum = 0;
+
+        for(int i = 0; i < nums2.length; i++) {
+            sum += nums2[i] + r.nextInt(100) + 1;
+        }
+
+        System.out.println(sum);
+
+        // 3.生成10个随机数,求平均数
+       int avgNum = (sum / nums2.length);
+
+       // 4.计算有多少数字小于平均数
+        int count = 0;
+        for (int i = 0; i < nums2.length; i++) {
+            if(avgNum > nums2[i]) count ++;
+        }
+
+        System.out.println(count);
+
+        // 5.交换数据
+        int[] nums3 = { 1, 2, 3, 4, 5 };
+
+        for(int i = 0, j = nums3.length - 1; i < j; i ++, j --) {
+            int temp = nums3[i]; // 1 2 3 4 5
+            nums3[i] = nums3[j]; // 5 4 3 2 1
+            nums3[j] = temp; // 1 2 3 4 5
+        }
+
+        for (int i = 0; i < nums3.length; i++) {
+            System.out.println(nums3[i]);
+        }
+
+        // 6.打乱数组
+        int[] num4 = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < num4.length; i++) {
+            int randomNumber = r.nextInt(num4.length);
+            num4[i] = num4[randomNumber];
+        }
+
+        for (int i = 0; i < num4.length; i++) {
+            System.out.println(num4[i]);
+        }
+    }
+}
